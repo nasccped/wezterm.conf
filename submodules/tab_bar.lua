b4bc1582ad457e56095b56c538dcd4689e4200f7
Nasccped <pedro.main.tec@gmail.com>
2025-11-23 11:44:02 -0300

    fix(move): move tab bar mod to UI

--- submodules/tab_bar.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
---- If tab bar is enabled
-local is_on = false
-
-M.enable_tab_bar = is_on
-return M
---
+++ submodules/ui.lua
@@ -0,0 +1,12 @@
+local M = {}
+--- If tab bar is enabled by default.
+local enable_tab_bar = false
+--- Window decor mode.
+local window_decorations = "RESIZE"
+--- Start full screen by default.
+local fullscreen = true
+
+M.enable_tab_bar = enable_tab_bar
+M.window_decorations = window_decorations
+M.fullscreen = fullscreen
+return M
