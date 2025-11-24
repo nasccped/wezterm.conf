0d34bde0640cbc31595ed58795245569dbfbffab
Nasccped <pedro.main.tec@gmail.com>
2025-11-24 14:23:05 -0300

    chore: add color scheme

--- submodules/ui.lua
+++ submodules/ui.lua
@@ -3,7 +3,10 @@ local M = {}
 local enable_tab_bar = false
 --- Window decor mode.
 local window_decorations = "RESIZE"
+--- My current colorscheme (intended to change)
+local color_scheme = "Galizur"
 
 M.enable_tab_bar = enable_tab_bar
 M.window_decorations = window_decorations
+M.color_scheme = color_scheme
 return M
