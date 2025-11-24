7218899f03d651b0318192e69bc1868a675c5df2
Nasccped <pedro.main.tec@gmail.com>
2025-11-24 13:28:18 -0300

    fix: remove fullscreen field config (placed at `wezterm.lua`)

--- submodules/ui.lua
+++ submodules/ui.lua
@@ -3,10 +3,7 @@ local M = {}
 local enable_tab_bar = false
 --- Window decor mode.
 local window_decorations = "RESIZE"
---- Start full screen by default.
-local fullscreen = true
 
 M.enable_tab_bar = enable_tab_bar
 M.window_decorations = window_decorations
-M.fullscreen = fullscreen
 return M
