87a761a4d08d8eca3a7cd0dc27934069e7766762
Nasccped <pedro.main.tec@gmail.com>
2025-11-24 14:24:05 -0300

    chore: call `submodules` tables + set main config

--- wezterm.lua
+++ wezterm.lua
@@ -1,89 +1,55 @@
 local wz = require("wezterm")
 local config = wz.config_builder()
 
---- Deeply extends the source table into the target table and then,
---- returns the new target.
----@param target table
----@param source table
+--- Operates the `require` function on a given path. Note that:
+--- 1. placing `"submodules"` at beginning isn't necessary since all
+---    submodules are within the `submodules` mod.
+--- 2. you must use forward slash (`/`) when passing the path instead
+---    dot (`.`) (wezterm doesn't allow unsafe C function call).
+---@param path string Submodule path to require.
 ---@return table
-local function deep_extend(target, source)
-  for k, v in pairs(source) do
-    if type(v) == "table" and type(target[k]) == "table" then
-      target[k] = deep_extend(target[k], v)
+local function submodule_require(path)
+  return require("submodules/" .. path)
+end
+
+-- load config submodules -------------------------------------------
+local utils = submodule_require("utils")
+
+--- Extends the local `config` table. The `content` param is expected
+--- to be a table, since it won't be pushed to the config, but will
+--- override the existing value, instead.
+---@param content table
+local function config_extend(content)
+  utils.panic_on(
+    type(content) ~= "table",
+    "config_extend: `content` param is expected to be a `table`"
+  )
+  for k, v in pairs(content) do
+    if type(config[k]) == "table" and type(v) == "table" then
+      config[k] = utils.table_deep_extend(config[k], v)
     else
-      target[k] = v
+      config[k] = v
     end
   end
-  return target
 end
 
---- Extends the config table by placing the provided value into the
---- designed field.
----@param field string
----@param value any
-local function extend(field, value)
-  if config[field] ~= nil then
-    config[field] = deep_extend(config[field], value)
-  else
-    config[field] = value
-  end
-end
+config_extend(submodule_require("fonts"))
+config_extend(submodule_require("keymaps"))
+config_extend(submodule_require("ui"))
 
----------------------------------------------------------------------
--- visual
----------------------------------------------------------------------
-extend("font", wz.font_with_fallback({
-  { family = "Iosevka Term",   weight = "DemiBold" },
-  { family = "Iosevka",        weight = "Bold" },
-  { family = "JetBrains Mono", weight = "Bold" },
-  { family = "Terminus",       weight = "Bold" },
-  "Noto Color Emoji",
-  "monospace",
-}))
-extend("font_size", 13.0)
-extend("colors", {
-  -- TODO: add ansy/bright colors
-  foreground = "#cccccc",
-  background = "#1e1e1e",
-  -- cursor colors
-  cursor_bg = "#cccccc",
-  cursor_fg = "#111111",
-  -- selection colors
-  selection_fg = "#1e1e1e",
-  selection_bg = "#ffffff",
-})
----------------------------------------------------------------------
--- key maps
----------------------------------------------------------------------
-extend("leader", {
-  key = "Space",
-  mods = "CTRL",
-  timeout_milliseconds = 1000
-})
-extend("keys", {
-  -- split pane horizontally by pressing LEADER + /
-  {
-    key = "/",
-    mods = "LEADER",
-    action = wz.action.SplitHorizontal({
-      domain = "CurrentPaneDomain"
-    })
-  },
-  -- split pane vertically by pressing LEADER + ?
-  {
-    key = "?",
-    mods = "LEADER",
-    action = wz.action.SplitVertical({
-      domain = "CurrentPaneDomain"
-    })
-  },
-  -- go copy mode by pressing LEADER + [
-  {
-    key = "[",
-    mods = "LEADER",
-    action = wz.action.ActivateCopyMode,
-  }
-})
--- TODO: add other key binds
+-- this var is used in the comment bellow. addressed mux to `_`, so
+-- lsp can stop arguing about it.
+local mux = wz.mux
+local _ = mux
+
+-- enable full screen on start up -----------------------------------
+-- Windows works not good at all with this (can't resize later +
+-- doesn't hide task bar) but performs good in MacOS and X11,
+-- according with wezterm author (https://github.com/wezterm/wezterm/issues/284#issuecomment-1177628870).
+-- -- consider uncommenting to enable it.
+-- wz.on("gui-startup", function()
+--   local _, _, window = mux.spawn_window{}
+--   window:gui_window():maximize()
+-- end)
 
 return config
