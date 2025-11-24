d6dad1545d668827b6dba9feec49ca0ba4069c04
Nasccped <pedro.main.tec@gmail.com>
2025-11-24 14:23:34 -0300

    chore: new key bind

--- submodules/keymaps.lua
+++ submodules/keymaps.lua
@@ -2,7 +2,7 @@ local M = {}
 local wz = require("wezterm")
 local action = wz.action
 local panic_on = require("submodules/utils/panic").panic_on
-local default_split_param = { domain = "CurrentPaneDomain" }
+local pane_domain = "CurrentPaneDomain"
 --- Default Timeout Milliseconds value.
 local timeout_m = 1000
 --- Default Leader Key.
@@ -14,8 +14,18 @@ local leader_key = {
 --- Stores the key maps to be added into wezterm `key` field.
 local key_maps = {}
 
+--- Push a new key table into the `key_maps` list.
+---@param t table
+function key_maps.push(t)
+  panic_on(
+    type(t) ~= "table",
+    "key_maps.push: `t` param is expected to be a table"
+  )
+  key_maps[#key_maps + 1] = t
+end
+
 --- Push a new key map into the `key_maps` table which operates with
---- the `LEADER` key. You can optionally pass another mod key
+--- the `LEADER` key. You can optionally add another mod key
 --- (through the `other_mod` param) like `LEADER` + `MOD` combo.
 ---@param key string
 ---@param other_mod? string
@@ -29,18 +39,63 @@ local function append_leader_combo(key, other_mod, act)
     other_mod == "LEADER",
     "append_leader_combo: `other_mod` can't be `LEADER` since it'll be a repeated combination"
   )
-  key_maps[#key_maps + 1] = {
+  local t = {
     key = key,
     mods = other_mod and other_mod .. "|LEADER" or "LEADER",
-    action = act,
+    action = act
   }
+  key_maps.push(t)
 end
 
---- pushing leader + key into key maps ------------------------------
-append_leader_combo("/", nil, action.SplitHorizontal(default_split_param))
-append_leader_combo("?", nil, action.SplitVertical(default_split_param))
+--- Key map that operates with no mods key (`mods = "NONE"`).
+--- Automatically pushes to `key_maps`.
+---@param key string
+---@param act any wezterm.action to be done.
+local function append_simple_key(key, act)
+  panic_on(
+    type(key) ~= "string",
+    "append_simple_key: `key` is expected to be a string"
+  )
+  local t = {
+    key = key,
+    mods = "NONE",
+    action = act
+  }
+  key_maps.push(t)
+end
+
+-- splitting --------------------------------------------------------
+-- horizontal
+append_leader_combo("|", "SHIFT", action.SplitHorizontal({ domain = pane_domain }))
+-- vertical
+append_leader_combo("\\", nil, action.SplitVertical({ domain = pane_domain }))
+
+-- tabs -------------------------------------------------------------
+-- create new
+append_leader_combo("t", nil, action.SpawnTab(pane_domain))
+for i = 0, 7 do
+  -- add `move-to` by the given index
+  append_leader_combo(tostring(i + 1), nil, action.ActivateTab(i))
+end
+-- index 9 go to last
+append_leader_combo("9", nil, action.ActivateTab(-1))
+-- close tab
+append_leader_combo("q", nil, action.CloseCurrentTab({ confirm = true }))
+
+-- copy mode --------------------------------------------------------
 append_leader_combo("[", nil, action.ActivateCopyMode)
 
+-- full screen ------------------------------------------------------
+append_simple_key("F11", action.ToggleFullScreen)
+
+-- setting keymap config
 M.leader = leader_key
-M.keys = key_maps
+M.keys = {}
+-- push key map by using loop (avoid pushing the `push` function
+-- declared at the top of the file)
+for _, m in ipairs(key_maps) do
+  if type(m) == "table" then
+    M.keys[#M.keys + 1] = m
+  end
+end
 return M
