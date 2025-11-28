ae20c3b96394dc1bb87b28240a9fca0daba4f93e
Nasccped <pedro.main.tec@gmail.com>
2025-11-28 19:53:13 -0300

    chore: update tab title returning

--- submodules/utils/tabs.lua
+++ submodules/utils/tabs.lua
@@ -1,13 +1,16 @@
 local M = {}
 
 --- Gets the title based on `TabInformation` value. If no title for
---- the given tab, it's `active_pane` title will be returned.
+--- the given tab, the default title (`"tab <TAB_INDEX + 1>"`) is
+--- returned.
 ---@param tab_info any
 ---@return string
 function M.tab_get_title(tab_info)
   local title = tab_info.tab_title
-  local default = tab_info.active_pane.title
-  return title and #title > 0 and title or default
+  local ind = tab_info.tab_index + 1
+  return type(title) == "string" and #title > 0
+      and string.format("%d: %s", ind, title)
+      or string.format("tab %d", ind)
 end
 
 return M
